c9a2684856285b5146f75935fe31136d92b42bae
minh <minh@coachingcloud.com>
2015-12-26 19:03:16 +0700

    update

--- Flea/AppDelegate.swift
+++ Flea/AppDelegate.swift
@@ -33,6 +33,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         Shop.registerSubclass()
         
         
+        // override select color of tabbard
+        UITabBar.appearance().tintColor = UIColor(red: 255/255.0, green: 187/255.0, blue: 0/255.0, alpha: 1.0)
+
+        
+        
         PFACL.setDefaultACL(defaultACL, withAccessForCurrentUser:true)
         PFFacebookUtils.initializeFacebookWithApplicationLaunchOptions(launchOptions)
         
