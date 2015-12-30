b4d5754838f5d21c18f5373025f2f4c453e2fc4c
datnguyen <datnguyen.personal@gmail.com>
2015-12-30 20:59:45 +0700

    refactoring storyboard

---
+++ Flea/NewShopCell.swift
@@ -0,0 +1,24 @@
+//
+//  NewShopCell.swift
+//  Flea
+//
+//  Created by Dat Nguyen on 30/12/15.
+//  Copyright © 2015 ThreeStrangers. All rights reserved.
+//
+
+import UIKit
+
+class NewShopCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
